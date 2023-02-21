653023263c7c4cc790c8adc7efed25fdc8dbb846
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2023-02-21 15:12:59 +0700

    fix: update index export useWatch & watchAll

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ export * from './useForm';
 export * from './useField';
 export * from './useView';
 export * from './useSubscribe';
+export * from './useWatch';
+export * from './useWatchAll';
 export * from './types';
 export * from './logic/createForm';
 export * from './logic/transform';
