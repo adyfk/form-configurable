815602cb99ec2db8b669dc3161a7f0a7c068468c
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2023-03-31 00:09:19 +0700

    fix: update root provider

---
+++ src/v2/components/FormRootProvider.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+const FormRootProvider: FC<{ children: any }> = ({ children }) => (
+  <DndProvider backend={HTML5Backend}>
+    {children}
+  </DndProvider>
+);
+
+export default FormRootProvider;
